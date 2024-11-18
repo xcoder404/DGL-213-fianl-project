b3aac0763e4c05a79240181bc3af21147007e0a6
xcoder404 <68740576+xcoder404@users.noreply.github.com>
2024-11-17 21:19:16 -0800

    Format all files

--- javascript/profile.js
+++ javascript/profile.js
@@ -12,16 +12,16 @@ changeAvatarBtn.addEventListener("click", () => {
 // Add event listener to the file input
 avatarInput.addEventListener("change", (event) => {
   const file = event.target.files[0];
-  
+
   // Check if a file is selected and if it's an image
   if (file && file.type.startsWith("image/")) {
     const reader = new FileReader();
-    
+
     // Once the file is read, set it as the source of the avatar image
     reader.onload = (e) => {
       avatarImage.src = e.target.result;
     };
-    
+
     reader.readAsDataURL(file);
   } else {
     alert("Please select a valid image file.");
